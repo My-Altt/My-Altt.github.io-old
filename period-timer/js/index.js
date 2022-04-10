24a813c1a20990d84a69a382b1d96a1b234737fc
My-Altt <101564533+My-Altt@users.noreply.github.com>
2022-04-10 15:55:22 -0400

    Update index.js

--- period-timer/js/index.js
+++ period-timer/js/index.js
@@ -1,4 +1,38 @@
-//This is so that the more info button works
+var x = setInterval(function() {
+  // Set the date we're counting down to
+console.log("I exist");
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = mm + '/' + dd + '/' + yyyy;
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  var countDownDate = new Date(today + " 02:30:00 PM").getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "N/A";
+  }
+}, 1000);
+
+//More info stuff
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
